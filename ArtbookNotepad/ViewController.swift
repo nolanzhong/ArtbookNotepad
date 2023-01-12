bb6eb5187b9b0a145df113f5a46f52f795462aeb
nolanzhong <nolan.zhong@duke.edu>
2023-01-12 00:34:09 -0500

    Initial Commit

---
+++ ArtbookNotepad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtbookNotepad
+//
+//  Created by Nolan Zhong on 1/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
